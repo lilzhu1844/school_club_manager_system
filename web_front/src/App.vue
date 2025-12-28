be2b83e2d4a2b4662bf7d0e0f76cc946c07291b0
Yeshenghui7389 <2642461476@qq.com>
2025-12-28 16:11:35 +0800

    修改

--- web_front/src/App.vue
+++ web_front/src/App.vue
@@ -1,5 +1,19 @@
 <template>
+<<<<<<< Updated upstream
   <RouterView />
+=======
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" /> 
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
+>>>>>>> Stashed changes
 </template>
 
 <script setup>
